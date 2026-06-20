0d8838bed0824276a86a9ec5dea73911fdbc9426
Karthik ks <karthikks902@gmail.com>
2026-06-20 09:48:32 +0530

    Added dynamic test data generator utility

---
+++ src/test/java/utils/TestDataGenerator.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class TestDataGenerator {
+
+    public static String generateUsername() {
+        return "user" + System.currentTimeMillis();
+    }
+
+    public static final String PASSWORD = "Password@123";
+
+}
